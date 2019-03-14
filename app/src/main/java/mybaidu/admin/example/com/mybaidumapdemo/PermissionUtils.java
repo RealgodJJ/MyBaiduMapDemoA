7f6510c4aa9dfaade214dba1507ee5517526ee9e
RealgodJJ <xiaozhiqiu13@sina.com>
2019-03-14 16:31:45 +0800

    用户设置禁止不再提示时，应提醒用户手动设置

--- app/src/main/java/mybaidu/admin/example/com/mybaidumapdemo/PermissionUtils.java
+++ app/src/main/java/mybaidu/admin/example/com/mybaidumapdemo/PermissionUtils.java
@@ -1,17 +1,22 @@
 package mybaidu.admin.example.com.mybaidumapdemo;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class PermissionUtils {
     private Activity activity;
-    private int requestCode;
-    private CallBack callBack;
+    private Map<Integer, CallBack> requestBack;
+//    private int requestCode;
+//    private CallBack callBack;
 
     public interface CallBack {
         void grandAll();
@@ -19,8 +24,10 @@ public class PermissionUtils {
         void denied();
     }
 
+    @SuppressLint("UseSparseArrays")
     public PermissionUtils(Activity activity) {
         this.activity = activity;
+        requestBack = new HashMap<>();
     }
 
     public void request(List<String> needPermissions, int requestCode, CallBack callBack) {
@@ -32,8 +39,9 @@ public class PermissionUtils {
         if (activity == null) {
             throw new IllegalArgumentException("Activity is null");
         }
-        this.requestCode = requestCode;
-        this.callBack = callBack;
+        requestBack.put(requestCode, callBack);
+//        this.requestCode = requestCode;
+//        this.callBack = callBack;
 
         //判断是否所有权限都已授权
         List<String> requestPermissions = new ArrayList<>();
@@ -52,20 +60,24 @@ public class PermissionUtils {
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (this.requestCode == requestCode) {
-            boolean grandAll = true;
-            for (int i = 0; i < grantResults.length; i++) {
-                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                    grandAll = false;
-                    Toast.makeText(activity, permissions[i] + "未授权", Toast.LENGTH_SHORT).show();
-                    break;
+        for (Integer key : requestBack.keySet()) {
+            if (key == requestCode) {
+                boolean grandAll = true;
+                for (int i = 0; i < grantResults.length; i++) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        grandAll = false;
+                        Toast.makeText(activity, permissions[i] + "未授权", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
                 }
-            }
 
-            if (grandAll) {
-                callBack.grandAll();
-            } else {
-                callBack.denied();
+                if (grandAll) {
+                    for (Integer request : requestBack.keySet())
+                        Objects.requireNonNull(requestBack.get(request)).grandAll();
+                } else {
+                    for (Integer request : requestBack.keySet())
+                        Objects.requireNonNull(requestBack.get(request)).denied();
+                }
             }
         }
     }
